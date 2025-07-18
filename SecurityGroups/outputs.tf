c208bfc5394d022019e7ee6c37129b4f765ab76c
favours-github <favourndudinwosu@gmail.com>
2025-07-19 00:26:33 +0100

    save

--- SecurityGroups/outputs.tf
+++ SecurityGroups/outputs.tf
@@ -1,15 +1,19 @@
-output "alb_security_group_name" {
-  value = aws_security_group.alb_security_group.name
+# export the alb security group id
+output "alb_security_group_id" {
+  value = aws_security_group.alb_security_group.id
 }
 
-output "bastion_security_group_name" {
-  value = aws_security_group.bastion_security_group.name
+# export the bastion security group id
+output "bastion_security_group_id" {
+  value = aws_security_group.bastion_security_group.id
 }
 
-output "app_server_security_group_name" {
-  value = aws_security_group.app_server_security_group.name
+# export the app server secuirty group id
+output "app_server_security_group_id" {
+  value = aws_security_group.app_server_security_group.id
 }
 
-output "database_security_group_name" {
-  value = aws_security_group.database_security_group.name
+# export the database security group id
+output "database_security_group_id" {
+  value = aws_security_group.database_security_group.id
 }
