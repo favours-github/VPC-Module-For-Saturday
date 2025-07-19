ca42b0ccc2e522aff4f5a871700ce73e44d467fa
favours-github <favourndudinwosu@gmail.com>
2025-07-19 04:22:53 +0100

    yes

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,44 +1,64 @@
+# export the region
+output "region" {
+  value = var.region
+}
+
+# export the project name
+output "project_name" {
+  value = var.project_name
+}
+
+# export the environment
+output "environment" {
+  value = var.environment
+}
+
+# export the vpc id
 output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.vpc.id
+  value = aws_vpc.vpc.id
 }
 
+# export the internet gateway
+output "internet_gateway" {
+  value = aws_internet_gateway.internet_gateway
+}
+
+# export the public subnet az1 id
 output "public_subnet_az1_id" {
-  description = "ID of the public subnet in availability zone 1"
-  value       = aws_subnet.public_subnet_az1.id
+  value = aws_subnet.public_subnet_az1.id
 }
 
+# export the public subnet az2 id
 output "public_subnet_az2_id" {
-  description = "ID of the public subnet in availability zone 2"
-  value       = aws_subnet.public_subnet_az2.id
+  value = aws_subnet.public_subnet_az2.id
 }
 
+# export the private app subnet az1 id
 output "private_app_subnet_az1_id" {
-  description = "ID of the private app subnet in availability zone 1"
-  value       = aws_subnet.private_app_subnet_az1.id
+  value = aws_subnet.private_app_subnet_az1.id
 }
 
+# export the private app subnet az2 id
 output "private_app_subnet_az2_id" {
-  description = "ID of the private app subnet in availability zone 2"
-  value       = aws_subnet.private_app_subnet_az2.id
+  value = aws_subnet.private_app_subnet_az2.id
 }
 
+# export the private data subnet az1 id
 output "private_data_subnet_az1_id" {
-  description = "ID of the private data subnet in availability zone 1"
-  value       = aws_subnet.private_data_subnet_az1.id
+  value = aws_subnet.private_data_subnet_az1.id
 }
 
+# export the private data subnet az2 id
 output "private_data_subnet_az2_id" {
-  description = "ID of the private data subnet in availability zone 2"
-  value       = aws_subnet.private_data_subnet_az2.id
+  value = aws_subnet.private_data_subnet_az2.id
 }
 
-output "internet_gateway_id" {
-  description = "The ID of the Internet Gateway"
-  value       = aws_internet_gateway.internet_gateway.id
+# export the first availability zone
+output "availability_zone_1" {
+  value = data.aws_availability_zones.available_zones.names[0]
 }
 
-output "public_route_table_id" {
-  description = "ID of the public route table"
-  value       = aws_route_table.public_route_table.id
+# export the second availability zone
+output "availability_zone_2" {
+  value = data.aws_availability_zones.available_zones.names[1]
 }
--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,51 +1,13 @@
-# Environment Variables
-variable "region" {
-  description = "The AWS region to deploy resources"
-  type        = string
-}
+# environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
 
-variable "project_name" {
-  description = "The name of the project"
-  type        = string
-}
-
-variable "environment" {
-  description = "The deployment environment dev)"
-  type        = string
-}
-
-# VPC Variables
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-}
-
-variable "public_subnet_az1_cidr" {
-  description = "CIDR block for public subnet in availability zone 1"
-  type        = string
-}
-
-variable "public_subnet_az2_cidr" {
-  description = "CIDR block for public subnet in availability zone 2"
-  type        = string
-}
-
-variable "private_app_subnet_az1_cidr" {
-  description = "CIDR block for private application subnet in availability zone 1"
-  type        = string
-}
-
-variable "private_app_subnet_az2_cidr" {
-  description = "CIDR block for private application subnet in availability zone 2"
-  type        = string
-}
-
-variable "private_data_subnet_az1_cidr" {
-  description = "CIDR block for private data subnet in availability zone 1"
-  type        = string
-}
-
-variable "private_data_subnet_az2_cidr" {
-  description = "CIDR block for private data subnet in availability zone 2"
-  type        = string
-}
+# vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
